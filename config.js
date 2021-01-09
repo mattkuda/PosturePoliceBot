4c786219fbd095bb26b6f5a4bf8bf1ca99867abd
Matthew Kuda <mattkuda@gmail.com>
2021-01-08 21:15:32 -0500

    Add code

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
-module.exports = {
-  consumer_key: 'y3HX36JDA7NCE2Fh49neURj8O',
-  consumer_secret: '9XMd996gLDIzMoKyNopg2DeLN6eTlms2nq479ZkoHNzbWQcHl7',
-  access_token: '1347612816409292812-CX6stsriV5Dnq0VxjH4YnIFic3igt0',
-  access_token_secret: 'YAVTXJFtdV9JD6FloVBt5Xk3W4Fueumqw33AaMZH9mQgF'
+require('dotenv').config();
 
-}
+module.exports = {
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
+
+};
+
+
+
